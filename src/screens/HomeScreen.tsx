3c55e76b02b5287e2e28bb3df0400e050ca346d8
Daiane Geraldino <129515871+DaianeGeraldino@users.noreply.github.com>
2023-12-12 16:17:09 -0300

    Update HomeScreen.tsx

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,6 +2,20 @@ import React, {useState} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { useStore } from '../store/store';
 
+const getCategoriesFromData = (data: any) => {
+  let temp:any = {};
+  for(let i=0; i<data.length; i++) {
+    if(temp[data[i].name] == undefined) {
+      temp[data[i].name] == 1;
+    } else {
+      temp[data[i].name] ++;
+    }
+  }
+  let categories = Object.keys(temp);
+  categories.unshift('All');
+  return categories;
+};
+
 const HomeScreen = () => {
   const CoffeeList = useStore((state: any) => state.CoffeeList)
   const BeanList = useStore((state: any) => state.BeanList)
@@ -12,7 +26,7 @@ const HomeScreen = () => {
     index:0,
     category: categories[0],
   });
-
+  const [sortedCoffee, setSortedCoffee] = useState(undefined);
   return (
     <View>
       <Text>HomeScreen</Text>

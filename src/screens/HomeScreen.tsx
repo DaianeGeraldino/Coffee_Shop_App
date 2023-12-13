587605fc83fca9702cd944c03c2df43fadb49f2e
Daiane Geraldino <129515871+DaianeGeraldino@users.noreply.github.com>
2023-12-13 17:03:44 -0300

    Update HomeScreen.tsx

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,9 @@
 import React, {useState} from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native'
 import { useStore } from '../store/store';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import { COLORS } from '../theme/theme';
+
 
 const getCategoriesFromData = (data: any) => {
   let temp:any = {};
@@ -16,24 +19,47 @@ const getCategoriesFromData = (data: any) => {
   return categories;
 };
 
+const getCoffeeList = (category: string,data:any) => {
+  if (category == "All") {
+    return data;
+  } else {
+    let coffeelist = data.filter((item:any) => item.name == category);
+    return coffeelist;
+  }
+};
+
 const HomeScreen = () => {
   const CoffeeList = useStore((state: any) => state.CoffeeList)
   const BeanList = useStore((state: any) => state.BeanList)
-
-  const [categories, setCategories] = useState([undefined]);
+  const [categories, setCategories] = useState(
+    getCategoriesFromData(CoffeeList),
+    );
   const [searchText, setSearchText] = useState(undefined);
   const [categoryIndex, setCategoryIndex] = useState({
     index:0,
     category: categories[0],
   });
-  const [sortedCoffee, setSortedCoffee] = useState(undefined);
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+  const [sortedCoffee, setSortedCoffee] = useState(
+    getCoffeeList(categoryIndex.category, CoffeeList),
+    );
 
-const styles = StyleSheet.create({})
+    const tabBarHeight = useBottomTabBarHeight();
+
+  return <View style={styles.ScreenContainer}>
+    <StatusBar backgroundColor={COLORS.primaryBlackHex}/>
+    <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.ScrollViewFlex}></ScrollView>
+  </View>;
+
+};
+
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  ScrollViewFlex: {
+    flex
+  }
+})
 
 export default HomeScreen
